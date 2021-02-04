10dd80115d35ad6b41965ab18a39f093ded56a24
Megha <meghagupta0891@gmail.com>
2021-02-04 16:30:50 +0000

    Added code to avoid sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,12 +7,14 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const values = [process.argv[3],`${process.argv[2]}%`];
+
 pool.query(`
 SELECT students.id as id, students.name as name, cohorts.name as cohort
 FROM students JOIN cohorts ON cohorts.id = cohort_id 
-WHERE cohorts.name LIKE '${process.argv[2]}%'
-LIMIT ${process.argv[3]};
-`)
+WHERE cohorts.name LIKE $2
+LIMIT $1;
+`,values)
 .then(res => {
   res.rows.forEach(row => {
       console.log(`${row.name} has id of ${row.id} and was in the ${row.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohort = process.argv[2];
+
+const values = [];
+values.push(`${cohort}`);
+
 pool.query(`
 select distinct teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances 
 from teachers
 join assistance_requests on teacher_id = teachers.id
 join students on student_id = students.id 
 join cohorts on cohort_id = cohorts.id
-where cohorts.name = '${process.argv[2]}'
+where cohorts.name = $1
 group by teachers.name, cohorts.name 
 order by teacher; 
-`)
+`,values)
 .then(res => {
   res.rows.forEach(row => {
       console.log(`${row.cohort}:${row.teacher}`);
